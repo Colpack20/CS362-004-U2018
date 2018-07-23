449b7d511f610db2429a999f3bd216b0be336101
Colpack20 <Colpack20@gmail.com>
2018-07-23 01:36:43 -0400

    Add files via upload

--- projects/reedkyl/dominion/cardtest1.c
+++ projects/reedkyl/dominion/cardtest1.c
@@ -50,26 +50,12 @@ int main() {
                 r = initializeGame(numPlayer, k, seed, &G); // initialize a new game set the number of cards on hand															 
 				// copy the game state to a test case
 				memcpy(&testcaseG, &G, sizeof(struct gameState));
-				
-				//cardEffect(smithy, choice1, choice2, choice3, &testcaseG, handpos, &bonus);
-				//testcaseG.hand[0][0] = smithy;
-				
-				//assert(testG.coins == G.coins + addedCoins);
-				//G.hand[1][0] = copper;
-				//G.hand[1][0] = copper;
-				//G.hand[1][0] = copper;
-				//G.hand[1][0] = copper;
-				
-				//G.handCount[1]++;
-				//G.handCount[1] = 1;
-				//G.discardCount[1] = 0;
-				
-				//G.whoseTurn = 1;
+
+				testcaseG.hand[0][0] = smithy;
+
 				int total = 0;
 				int h;
-				//printf("Player 1's hand is %d, their deck is %d, and their discard pile is %d \n", testcaseG.handCount[0], testcaseG.deckCount[0], testcaseG.discardCount[0]);
-				//printf("Player 2's hand is %d, their deck is %d, and their discard pile is %d \n", testcaseG.handCount[1], testcaseG.deckCount[1], testcaseG.discardCount[1]);
-				//printf("deck count is %d\n", G.deckCount[1]);//should be 11
+
 				int firstDeckcount = G.deckCount[0];
 				for (h = 0; h < 27; h++)	
 					total += fullDeckCount(0, h, &G);
@@ -136,20 +122,7 @@ int main() {
 					printf("Test failed, %d card(s) was/were trashed unnecessarily from Player 1's full deck\n", total-newtotal);
 				else
 					printf("Test failed, some cards were added unnecessarily to Player 1's full deck\n");
-				//printf("test 1C: were three cards drawn from the deck successfully?\n");
-				//printf("initial deckCount is %d and then deck count is %d\n", firstDeckcount, G.deckCount[1]);
-				//if(testcaseG.deckCount[0] == (firstDeckcount - 3))
-					//printf("test passed, three cards were drawn from the deck successfully\n");
-				//else
-					//printf("test failed, three cards weren't drawn from the deck successfully, %d cards were drawn\n", firstDeckcount-G.deckCount[1]);
-				/*
-				printf("test 1D: did Player 1's state (hand, deck, and discard) change at all unnecessarily? Assuming that Player 1 started with 5 cards in their deck, 5 cards in their hand, and 0 cards in the discard pile)\n");
-				printf("Player 1's hand is %d, their deck is %d, and their discard pile is %d \n", G.handCount[0], G.deckCount[0], G.discardCount[0]);
-				printf("Player 2's hand is %d, their deck is %d, and their discard pile is %d \n", G.handCount[1], G.deckCount[1], G.discardCount[1]);				
-				printf("kingdom cards is %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7], k[8], k[9]);   */
-				//if
-				
-				//else
+
 	
 
     printf("All tests taken!\n");
